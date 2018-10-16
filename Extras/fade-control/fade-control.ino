60e334e8641084e2e9dee5dd0ed8cdeba852c377
Stephanie Smith <stephiesmith@gmail.com>
2018-10-15 19:22:10 -0700

    minor changes

--- Extras/fade-control/fade-control.ino
+++ Extras/fade-control/fade-control.ino
@@ -3,6 +3,9 @@ int ledPin = 10; // use a PWM pin
 int knobPin = A0;
 int knobValue = 0;
 
+int sensorPin = A1;
+int sensorValue = 0;
+
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
@@ -10,20 +13,20 @@ void setup() {
 
 void loop() {
 
-  knobValue = analogRead(knobPin); // read the knob
+  sensorValue = analogRead(sensorPin); // read the knob
 
 
 // constrain range of knob
-  knobValue = constrain(knobValue, 0, 655);
-  Serial.print("Knob: ");
-  Serial.print(knobValue); // see what values you get from knobPin
+  //knobValue = constrain(knobValue, 0, 655);
+  Serial.print("Sensor: ");
+  Serial.print(sensorValue); // see what values you get from knobPin
   
 //  Map the values
-  knobValue = map(knobValue, 0, 655, 0, 255); // match the range of the PWM pin
-  Serial.print("Knob Mapped: ");
-  Serial.println(knobValue); // see what values you get from knobPin
+  sensorValue = map(sensorValue, 320, 380, 0, 255); // match the range of the PWM pin
+  Serial.print("Sensor Mapped: ");
+  Serial.println(sensorValue); // see what values you get from knobPin
   
-  analogWrite(ledPin, knobValue); // use analogWrite with PWM pins
+  analogWrite(ledPin, sensorValue); // use analogWrite with PWM pins
   
 
 }
--- Extras/sensor-reader/sensor-reader.ino
+++ Extras/sensor-reader/sensor-reader.ino
@@ -10,7 +10,7 @@
  
 **************************************/
 
-int ledPin = 13; // same as pin 13
+int ledPin = 10; // same as pin 13
 
 int sensorPin = A0;     // analog pin because reading a range of numbers
 int sensorValue = 0;    // variable for reading the sensor
@@ -29,13 +29,48 @@ void loop() {
   // main program goes here
   
   sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);      // print out the value
-  delay(50);
-  // visual indicator
-//  digitalWrite(ledPin, HIGH);       // turn the ledPin on
-//  delay(sensorValue);                 // stop the program for <knobValue> ms
-//  sensorValue = analogRead(sensorPin);
-//  digitalWrite(ledPin, LOW);        // turn the ledPin off
-//  delay(sensorValue);                 // stop the program for <knobValue> ms
+  //Serial.println(sensorValue);      // print out the value
+  delay(10);
+  sensorValue = map(sensorValue, 0, 600, 0, 400);
+
+
+//  if (sensorValue > 0 && sensorValue < 100) {
+//    Serial.print(sensorValue);
+//    Serial.println(" test1");
+//  } else if (sensorValue > 100 && sensorValue < 200) {
+//    Serial.print(sensorValue);
+//    Serial.println(" test2");
+//  } else if (sensorValue > 200 && sensorValue < 300) {
+//    Serial.print(sensorValue);
+//    Serial.println(" test3");
+//  }
+
+
+  // change intensity of motor
+  //analogWrite(ledPin, sensorValue);
+
+  // change on/off (blink) rate
+  digitalWrite(ledPin, HIGH);       // turn the ledPin on
+  delay(sensorValue);                 // stop the program for <knobValue> ms
+  sensorValue = analogRead(sensorPin);
+  digitalWrite(ledPin, LOW);        // turn the ledPin off
+  delay(sensorValue);                 // stop the program for <knobValue> ms
+
+
+
+
+
+//  if (0 < sensorValue < 100) {
+//    Serial.print(sensorValue);
+//    Serial.println(" test1");
+//  } else if ( 100 < sensorValue < 200) {
+//    Serial.print(sensorValue);
+//    Serial.println(" test2");
+//  } else if (200 < sensorValue < 300) {
+//    Serial.print(sensorValue);
+//    Serial.println(" test3");
+//  }
+  
+
 
 }
